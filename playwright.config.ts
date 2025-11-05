78819d68d0eb4605954fd6378d4bcef58b5219b4
Razvan Vancea <razvanvancea94@gmail.com>
2025-11-05 19:41:11 +0200

    add blob reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['html'],['list']],
+  reporter: [['blob'],['html'],['list']],
   use: {
     testIdAttribute: 'test-data',
     trace: 'retain-on-failure',
